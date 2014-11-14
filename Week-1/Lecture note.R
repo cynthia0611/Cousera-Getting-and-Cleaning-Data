9dcaf8bd9125fd36875dbfdc2e359cb8767e6f55
Cynthia Sun <cynthia0828@gmail.com>
2014-11-14 01:39:29 -0500

    Update Lecture note.R

--- Week-1/Lecture note.R
+++ Week-1/Lecture note.R
@@ -98,3 +98,73 @@ cat(myjson)
 iris2<- fromJSON(myjson)
 class(iris2)#data frame
 head(iris2)
+----------------------------------------------------------------------------------------------------------------------------
+
+## Using data.table
+library(data.table)
+DF <- data.frame(x=rnorm(9), y=rep(c("a","b","c"), each=3), z=rnorm(9))
+head(DF, 3)
+DT <- data.table(x=rnorm(9), y=rep(c("a","b","c"), each=3), z=rnorm(9))
+head(DT, 3)
+# get the general situation of this table
+tables()
+DT[2, ]
+DT[DT$y=="a", ]
+DT[c(2, 3)]
+DT[, c(2,3)] # not subsetting the columns
+# expression
+{
+  x = 1
+  y = 2
+}
+k = {print(10); 5} # 10
+print(k) # 5
+
+# summary table
+DT[, list(mean(x), sum(z))]
+DT[, table(y)]
+
+# adding new columns
+DT[, w:=z^2]
+DT
+DT2 <- DT
+DT[, y:=2]
+DT
+DT2 # it is changed as well
+head(DT, n=3)
+
+# multiple operations
+DT[, m:={tmp <- (x+z); log2(tmp+5)}]
+DT
+DT2 # it is changed as well; they point to the same address
+
+# plyr like operations
+DT[, a:=x>0]
+DT
+
+DT[, b:=mean(x+w), by=a]#group by a which is true/false
+DT
+
+# .N an integer, length 1, containing the number  
+set.seed(123)
+DT <- data.table(x=sample(letters[1:3], 1E5, TRUE))
+DT[, .N, by=x]
+
+# keys
+DT <- data.table(x=rep(c("a", "b", "c"), each=100), y=rnorm(300))
+setkey(DT, x)
+DT['a']
+
+# use keys to do joins
+DT1 <- data.table(x=c('a', 'a', 'b', 'dt1'), y=1:4)
+DT2 <- data.table(x=c('a', 'b', 'dt2'), z=5:7)
+setkey(DT1, x) 
+setkey(DT2, x)
+merge(DT1, DT2)
+
+# use keys to fast reading
+big_df <- data.frame(x=rnorm(1E6), y=rnorm(1E6))
+file <- tempfile()
+write.table(big_df, file=file, row.names=FALSE, col.names=TRUE, sep="\t", quote=FALSE)
+system.time(fread(file))
+system.time(read.table(file, header=TRUE, sep="\t")) # so slow
