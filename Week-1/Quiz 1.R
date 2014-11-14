457cb6d6b0f185ae5462b9f425fdc2e8243e074c
Cynthia Sun <cynthia0828@gmail.com>
2014-11-14 18:45:25 -0500

    Update Quiz 1.R

--- Week-1/Quiz 1.R
+++ Week-1/Quiz 1.R
@@ -44,10 +44,16 @@ sum(xpathSApply(rootNode, "//zipcode", xmlValue)==21231)
 # 127
 
 # Qeustion 5
-
-
-# Qeustion 6
-
-# Qeustion 7
-
-# Qeustion 8
+library(data.table)
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv"
+download.file(fileUrl, destfile="./microdata3.csv")
+# fread() is a function with the data.table packge
+DT <- fread("./microdata3.csv")
+file.info("./microdata3.csv")$size
+system.time(DT[,mean(pwgtp15),by=SEX])
+system.time(mean(DT[DT$SEX==1,]$pwgtp15))+system.time(mean(DT[DT$SEX==2,]$pwgtp15))
+system.time(sapply(split(DT$pwgtp15,DT$SEX),mean))
+system.time(mean(DT$pwgtp15,by=DT$SEX))
+system.time(tapply(DT$pwgtp15,DT$SEX,mean))
+system.time(rowMeans(DT)[DT$SEX==1])+system.time(rowMeans(DT)[DT$SEX==2]
+)
